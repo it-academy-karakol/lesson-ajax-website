8c725246a51ceea52be6e85399b3b0ff2071cebe
Elaman Imashov <elaman@outlook.com>
2019-02-05 00:07:26 +0600

    Handle possible errors

--- main.js
+++ main.js
@@ -5,22 +5,42 @@ const title = document.getElementById('title');
 for (let index = 0; index < navLinks.length; index++) {
   const navLink = navLinks[index];
 
+  updateContent("Loading...", "Loading...");
+
   navLink.onclick = function(event) {
     const target = event.srcElement.href.split('#').pop();
 
     const xhr = new XMLHttpRequest();
     xhr.open('GET', '/data/' + target + '.json');
     xhr.send();
+
+    // Handle connection error
+    xhr.onerror = function() {
+      updateContent('Internet connection error...');
+    };
     xhr.onload = function() {
+      // Handle server error
+      if (this.status != 200) {
+        updateContent('Error retrieving content...');
+        return;
+      }
+
       try {
         const data = JSON.parse(xhr.responseText);
 
-        title.innerHTML = data.title;
-        content.innerHTML = data.content;
+        updateContent(data.title, data.content);
       }
-      catch {}
-    }
+      catch {
+        // Handle data error
+        updateContent('Error parsing content...');
+      }
+    };
   }
+}
+
+function updateContent(newTitle, newContent = '') {
+  title.innerHTML = newTitle;
+  content.innerHTML = newContent;
 }
 
 navLinks[0].click();
